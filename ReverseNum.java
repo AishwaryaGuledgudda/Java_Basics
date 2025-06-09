7482d48782d1fcca4e236ddaa9b5f6f3d5df3050
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-06-09 12:49:24 +0100

    Reverse a Number

--- ReverseNum.java
+++ ReverseNum.java
@@ -4,8 +4,7 @@ public class ReverseNum {
     StringBuffer NumReverse(int a){
         //Convert the given number to stringbuffer class to use revrese method
         StringBuffer sb = new StringBuffer(String.valueOf(a));
-        StringBuffer rev=sb.reverse();
-        return rev;
+        return sb.reverse();
 
     }
 
@@ -14,7 +13,7 @@ public class ReverseNum {
         System.out.println("Enter the number to be revresed");
         int num = scanner.nextInt();
         ReverseNum rn = new ReverseNum();
-        StringBuffer reversed = rn.NumReverse(num);
+        rn.NumReverse(num);
         System.out.println("The revresed number is " +num);
 
     }
