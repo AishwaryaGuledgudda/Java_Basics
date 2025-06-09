9adb3480f2bdf382eb387a3067674c9606dce99f
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-06-09 13:26:04 +0100

    Reverse a Number

---
+++ DigitCount.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DigitCount {
+    static int countDigi(int num){
+        StringBuffer str = new StringBuffer(String.valueOf(num));
+        int count =str.length();
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int input = scanner.nextInt();
+        int count =  countDigi(input);
+        System.out.println("The number if digit present in the input number is " +count);
+    }
+}
