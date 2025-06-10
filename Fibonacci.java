d681883d3ccb30bae5883ef55152fff8676dcc65
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-06-10 15:26:07 +0100

    Fibonacci

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,8 +5,8 @@ public class Fibonacci {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number till where fib should be generated");
         int n = scanner.nextInt();
-        int n1=0,n2=1,sum=0;
-        System.out.print(+n1+" "+n2);
+        int n1=0,n2=1,sum;
+        System.out.print(n1+" "+n2);
         for(int i=0;i<=(n-3);i++){ // (n-3) is imp because we are already printing two first digits
             sum=n1+n2;
             n1=n2;
