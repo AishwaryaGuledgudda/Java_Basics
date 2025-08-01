23d11db567085ab92ee94afb308e986eb6daca79
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-08-01 15:55:34 +0100

    swap two numbers

--- YT_SwapTwoNO.java
+++ YT_SwapTwoNO.java
@@ -1,3 +1,16 @@
+import java.util.Arrays;
+
 public class YT_SwapTwoNO {
 
+    //Swap using bitwise manipulation
+    public static void main(String[] args) {
+        int a=15,b=10;
+        System.out.println("a= " +a+ " " +"b= "+b );
+        a = a^b;
+        System.out.println(a);
+        b=a^b;
+        System.out.println("b= " +b);
+        a=a^b;
+        System.out.println("a= " +a);
+    }
 }
