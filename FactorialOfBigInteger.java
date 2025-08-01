0d29df388ebb9869725af55a6c72f5df539b7288
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-08-01 15:56:08 +0100

    Factorial of Big num

---
+++ FactorialOfBigInteger.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+
+import static java.lang.String.valueOf;
+
+public class FactorialOfBigInteger {
+
+    public static void main(String[] args) {
+        int a = 50;
+        BigInteger fact = BigInteger.ONE;
+        for(int i=1;i<a;i++) {
+           // fact=fact*i;
+            fact = fact.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println(fact);
+    }
+
+}
